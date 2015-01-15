1edee8e24a4c83408237ca1b358ae8a72fe3d04e
dydengbin <dydengbin@qq.com>
2015-01-15 16:12:33 +0800

    1、跟新了TMTeacherInfoHandler.ashx

--- DB.BPM.Admin/TM/ashx/TMTeacherInfoHandler.ashx
+++ DB.BPM.Admin/TM/ashx/TMTeacherInfoHandler.ashx
@@ -1,1 +1,1 @@
-<%@ WebHandler Language="C#" CodeBehind="TMTeacherInfoHandler.ashx.cs" Class="DB.BPM.Admin.TM.ashx.TMTeacherInfoHandler" %>
+﻿<%@ WebHandler Language="C#" CodeBehind="TMTeacherInfoHandler.ashx.cs" Class="DB.BPM.Admin.TM.ashx.TMTeacherInfoHandler" %>
--- DB.BPM.Admin/TM/ashx/TMTeacherInfoHandler.ashx.cs
+++ DB.BPM.Admin/TM/ashx/TMTeacherInfoHandler.ashx.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +16,7 @@ namespace DB.BPM.Admin.TM.ashx
     /// <summary>
     /// dbHandler 的摘要说明
     /// </summary>
-    public class TMTeacherInfoHandler : IHttpHandler,IRequiresSessionState
+    public class TMTeacherInfoHandler : IHttpHandler, IRequiresSessionState
     {
         public void ProcessRequest(HttpContext context)
         {
